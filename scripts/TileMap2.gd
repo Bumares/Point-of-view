e31a754a799130e6e8758e84086a3136ceca3edc
Bumares <76940606+Bumares@users.noreply.github.com>
2021-03-04 16:03:38 +0100

    scena

---
+++ scripts/TileMap2.gd
@@ -0,0 +1,29 @@
+extends TileMap
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+# Called when the node enters the scene tree for the first time.
+func _physics_process(delta):
+	if Input.is_action_just_pressed("E")&& self.visible == true:
+		self.visible = false
+	else:
+		if Input.is_action_just_pressed("E")&& self.visible == false:
+			self.visible = true
+	if Input.is_action_just_pressed("ui_left"):
+		self.visible = false
+		
+	if Input.is_action_just_pressed("ui_right"):
+		self.visible = false
+	if Input.is_action_just_pressed("ui_select"):
+		self.visible = false
+	if Input.is_action_just_pressed("ui_down"):
+		self.visible = false
+	if Input.is_action_just_pressed("ui_up"):
+		self.visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
